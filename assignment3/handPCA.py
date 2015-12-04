b7bab71b3a3bd5b6ec23a9a95067db04f2b05af4
Mads Thoudahl <thoudahl@gmail.com>
2015-12-04 09:49:52 +0100

    3rd window active

--- assignment3/handPCA.py
+++ assignment3/handPCA.py
@@ -8,6 +8,7 @@ hands     = np.loadtxt('hands.csv', delimiter=',')
 
 # Perform PCA
 hands_pca = PCA().fit_transform(hands)
+hands_pcs = PCA().fit(hands)
 
 # If there are fewer points than dimensions the PCA will automatically remove some columns. 
 # They're not needed, but for completeness I readd them
@@ -15,5 +16,20 @@ if hands.shape[0]<hands.shape[1]:
     hands_pca = np.append(hands_pca, np.zeros( (hands.shape[0],hands.shape[1]-hands.shape[0])),axis=1)
 
 # Store PCA
-np.savetxt('hands_pca.csv',hands_pca,fmt="%11.8f", delimiter=',')
+np.savetxt('hands_pca.csv',hands_pca, fmt="%11.8f", delimiter=',')
 
+sigma = np.sqrt(hands_pcs.explained_variance_)
+
+n = 8
+synhand = np.zeros((2+2*n,112))
+synhand[0] = hands_pcs.mean_
+synhand[1,:len(sigma)] = sigma/2
+
+xs = np.zeros((112,40))
+for i in range(n):
+    xs[i]   =  sigma[i]
+    xs[i+n] = -sigma[i]
+
+synhand[2:] = hands_pcs.inverse_transform(xs)[:2*n]
+
+np.savetxt('hands_syn.csv',synhand, fmt="%11.8f", delimiter=',')
