5ccc5a46d735acf5a4c74848f034c9036e5d43d3
Thorbjørn Christensen <cvcv90@gmail.com>
2015-12-09 08:38:49 +0100

    Districts and crimes are now separated into SVG groups

--- assignment4/ass4.js
+++ assignment4/ass4.js
@@ -21,74 +21,74 @@ function init(){
     
     ass3.geoPath = d3.geo.path().projection(ass3.projection);
 
-    d3.json("sfpd_districts.geojson", buildDistrictMap);
+    d3.json("sfpd_districts.geojson", function(json){
+        d3.csv("population.csv", function(csv_data) {
+            ass3.policeDistricts = json.features;
+            ass3.policeDistricts.forEach(function(district){
+                pop_data = csv_data.filter(function (d) {
+                    return d.District === district.properties.district;
+                })[0];
+                district.properties.population = pop_data;
+            });
+            buildDistrictMap();
+        });
+    });
 
 
 }
 
 function buildDistrictMap(json) {
-
-    ass3.policeDistricts = json.features;
-
     var colors = colorbrewer.Set3[ass3.policeDistricts.length];    
 
     ass3.svg1.selectAll("path")
         .data(ass3.policeDistricts)
         .enter()
-        .append("path")
-        .attr("d", ass3.geoPath)
-        .style("stroke", "orange")
-        .style("stroke-width", 0)
-        .style("fill", function(d, i) {
-            return colors[i]
-        })
-        .each(function(d){
+	.append("g")
+	.each(function(d){
             var _this = d3.select(this);
             _this.on("mouseover", function() {
                 // Move to front (to ensure the stroke isn't overlapped by anything)
                 this.parentNode.appendChild(this);
-
-                ass3.dict_to_crimes[d.properties.district]
-                    .forEach(function(crime){
-                        // Move each crime in this area to the front
-                        crime.parentNode.appendChild(crime);
-                    });
-                // Set stroke width
-                d3.select(this).style("stroke-width",3);
-            })
-                .on("mouseout", function() {
-                    d3.select(this).style("stroke-width",0);
-                });
+                d3.select(this).select("path").style("stroke-width",2);
+            });
+            _this.on("mouseout", function() {
+                d3.select(this).select("path").style("stroke-width",0);
+            });
         })
+	.attr("id", function(d) { return d.properties.district })
+        .append("path")
+        .attr("d", ass3.geoPath)
+        .style("stroke", "black")
+        .style("stroke-width", 0)
+        .style("fill", function(d, i) {
+            return colors[i]
+        });
 
     d3.json("sf_crime.geojson", buildCrimes);
 }
 
 function buildCrimes(json) {
-    ass3.crime_circles = ass3.svg1.selectAll("circle")
-        .data(json.features)
-        .enter()
-        .append("circle")
-    /* Use each() instead of setting the attributes directly,
-       to avoid multiple calls to the projection function for each point. */
-        .each(function(d){
-            var _this = d3.select(this);
-            coords = ass3.projection(d.geometry.coordinates);
-            _this.attr("cx", coords[0]);
-            _this.attr("cy", coords[1]);
-        })
-            .attr("r", 1)
-
-    ass3.dict_to_crimes = {}
     for(var i = 0; i < ass3.policeDistricts.length; i++){
         var dist = ass3.policeDistricts[i];
         var district_name = dist.properties.district;
-        console.log(district_name);
-        var _aa = ass3.crime_circles.filter(function(cc){
-            return cc.properties.PdDistrict === district_name;
-        });
-        ass3.dict_to_crimes[district_name] = _aa[0];
-    }
+	var filtered = json.features.filter(function(crime) {
+	    return crime.properties.PdDistrict === district_name;
+	});
+	d3.select("#" + district_name)
+	    .selectAll("circle")
+	    .data(filtered)
+	    .enter()
+            .append("circle")
+	/* Use each() instead of setting the attributes directly,
+	   to avoid multiple calls to the projection function for each point. */
+            .each(function(d){
+		var _this = d3.select(this);
+		coords = ass3.projection(d.geometry.coordinates);
+		_this.attr("cx", coords[0]);
+		_this.attr("cy", coords[1]);
+            })
+		.attr("r", 1)
+    }	
 }
 
     
