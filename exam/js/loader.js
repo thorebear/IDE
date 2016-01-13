f4bfd2b1d3165836ca44c2cb2a1de0c31d868c27
Thorbjørn Christensen <cvcv90@gmail.com>
2016-01-13 11:16:54 +0100

    path in loader.js

--- exam/js/loader.js
+++ exam/js/loader.js
@@ -1,7 +1,7 @@
 wc = {}
 
 $(document).ready(function() {
-    d3.json("../data/wc_day44_1_new.json", function(error, json) {
+    d3.json("data/wc_day44_1_new.json", function(error, json) {
 	if (error) return console.warn(error);
 
 	wc.hourData = json;
