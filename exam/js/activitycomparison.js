3ee02aab2ce766efb9d14dfb0a1cb476e588ab57
Mads Thoudahl <thoudahl@gmail.com>
2016-01-19 14:09:36 +0100

    comparison graph, done

--- exam/js/activitycomparison.js
+++ exam/js/activitycomparison.js
@@ -2,75 +2,48 @@
 function create_activity_comparison(data, parameter, color, maxValue){
     wc.ac_margin_left = 100;
     wc.ac_margin_top = 60;
+    wc.ac_margin_right = 0;
+    wc.ac_margin_bottom = 60;
     
-    var margin_right = 00,
-        margin_bottom = 60,
-        boxPadding = 4;
-
     var nTicks = 5;
 
     var ac_svg = d3.select("#activity_comparison");
+    wc.ac_svg_width  = parseInt(ac_svg.style("width"));
+    wc.ac_svg_height = parseInt(ac_svg.style("height"));
 
-    var svg_width  = parseInt(ac_svg.style("width"));
-    var svg_height = parseInt(ac_svg.style("height"));
-
-    wc.ac_width    = svg_width - margin_left - margin_right,
-    wc.ac_height   = svg_height - wc.ac_margin_top - margin_bottom;
+    var xcenter = wc.ac_margin_left + (wc.ac_svg_width - (wc.ac_margin_left + wc.ac_margin_right))/2 
 
     // use input from data
     var avg      = data.average,
         sum      = data.sum,
         weekdata = data.parameter;
 
+    // to calculate local variables ...
     var weeks  = weekdata.map(function(obj){return obj.week}),
         vals   = weekdata.map(function(obj){return obj.parameter}),
         btitle = wc.dayScale(data.day) + " " + data.hour + "-"+ (data.hour+1),
-        ctitle = "Compared to "+wc.dayScale(data.day) + " " + data.hour + "-"+ (data.hour+1);
-    
+        ctitle = wc.dayScale(data.day) + " " + data.hour + "-"+ (data.hour+1);
+
+    // ... and to calculate stuff available in the 'wc' namespace
     wc.weekScale = d3.scale.ordinal()
       .domain(d3.range(weekdata.length))
       .range(weeks);
 
     wc.ac_xScale = d3.scale.ordinal()
       .domain(wc.weeks)
-      .rangeRoundBands([wc.ac_margin_left, svg_width- margin_right], 0.6 );
+      .rangeRoundBands([wc.ac_margin_left, wc.ac_svg_width - wc.ac_margin_right], 0.6 );
 
     wc.ac_yScale = d3.scale.linear()
       .domain([0, maxValue])
-      .range([svg_height-margin_bottom, wc.ac_margin_top]);
+      .range([wc.ac_svg_height - wc.ac_margin_bottom, wc.ac_margin_top]);
 
-    wc.ac_offset = 30; 
-    wc.ac_offset = (svg_width - (margin_right + wc.ac_margin_left))*(1-0.6)/(wc.nWeeks*2); 
-
-    console.log(wc.ac_offset)
-    // SVG alterations
-    // titles
-    ac_svg.select('.btitle').remove();
-    ac_svg.append('g').attr('class', 'btitle');
-    ac_svg.select('.btitle')
-      .append('text')
-      .attr('x', (wc.ac_margin_left + wc.ac_offset))
-      .attr('y', wc.ac_margin_top)
-      .attr('fill', getColor(parameter))
-      .attr('text-anchor', 'left')
-      .text( btitle);
-
-    ac_svg.select('.ctitle').remove();
-    ac_svg.append('g').attr('class', 'ctitle');
-    ac_svg.select('.ctitle')
-      .append('text')
-      .attr('x', (2*wc.ac_margin_left + 2*wc.ac_offset))
-      .attr('y', wc.ac_margin_top)
-      .attr('fill', getColor(parameter))
-      .attr('text-anchor', 'left')
-      .text( ctitle);
-
+    wc.ac_offset = (wc.ac_svg_width - (wc.ac_margin_right + wc.ac_margin_left))*(1-0.6)/(wc.nWeeks*2); 
 
     // bars and their alignment
     // primary selected interval
-    ac_svg.append('g').attr('class', 'base');
+    ac_svg.append('g').attr('class', 'base bar');
 
-    ac_base = ac_svg.select('.base')
+    ac_base = ac_svg.select('.base.bar')
       .selectAll('rect')
       .data(weekdata)
 
@@ -81,7 +54,6 @@ function create_activity_comparison(data, parameter, color, maxValue){
       
     ac_base.enter()
       .append('rect')
-      .attr('class', 'bar')
       .attr('x', function(d){ return wc.ac_xScale(d.week) - wc.ac_offset} )
       .attr('y', function(d){ return  wc.ac_yScale(d.parameter)} )
       .attr('height', function(d){ return wc.ac_yScale(0) - wc.ac_yScale(d.parameter)})
@@ -92,10 +64,11 @@ function create_activity_comparison(data, parameter, color, maxValue){
 
     ac_base.exit()
       .remove()
-    // secondary interval, shown on hover
-    ac_svg.append('g').attr('class', 'comp');
 
-    ac_comp = ac_svg.select('.comp')
+    // secondary interval, shown on hover
+    ac_svg.append('g').attr('class', 'comp bar');
+
+    ac_comp = ac_svg.select('.comp.bar')
       .selectAll('rect')
       .data(weekdata)
 
@@ -106,7 +79,6 @@ function create_activity_comparison(data, parameter, color, maxValue){
 
     ac_comp.enter()
       .append('rect')
-      .attr('class', 'bar')
       .attr('x', function(d){ return wc.ac_xScale(d.week) + wc.ac_offset } )
       .attr('y', function(d){ return  wc.ac_yScale(d.parameter)} )
       .attr('width', wc.ac_xScale.rangeBand() )
@@ -127,13 +99,20 @@ function create_activity_comparison(data, parameter, color, maxValue){
     var yAxis = d3.svg.axis()
       .scale(wc.ac_yScale)
       .orient('left')
+      .tickFormat(function(d) { return d3.format("s")(d) })
       .ticks(nTicks);
 
+    // redraw for every click... remove and recreate placeholders
+    ac_svg.selectAll('.label').remove();
     ac_svg.selectAll('.axis').remove();
 
+    ac_svg.append('g').attr('class', 'label base');
+    ac_svg.append('g').attr('class', 'label comp');
+
+    // draw axes
     ac_svg.append('g')
         .attr('class', 'x axis')
-        .attr('transform', 'translate( 0, '+(svg_height-margin_bottom)+')')
+        .attr('transform', 'translate( 0, '+(wc.ac_svg_height-wc.ac_margin_bottom)+')')
       .call(xAxis);
 
     ac_svg.append('g')
@@ -141,6 +120,76 @@ function create_activity_comparison(data, parameter, color, maxValue){
         .attr('transform', 'translate('+(wc.ac_margin_left-2)+', 0)')
       .call(yAxis);
 
+    // titles and labels as well
+    ac_svg.select('.label.base')
+      .append('text')
+      .attr('x', (wc.ac_margin_left + wc.ac_offset))
+      .attr('y', wc.ac_margin_top)
+      .attr('fill', getColor(parameter))
+      .attr('text-anchor', 'start')
+      .text( btitle);
+
+    ac_svg.select('.label.comp')
+      .append('text')
+      .attr('x', (wc.ac_svg_width - (wc.ac_margin_right + wc.ac_offset)) )
+      .attr('y', wc.ac_margin_top)
+      .attr('fill', getColor(parameter))
+      .attr('text-anchor', 'end')
+      .text( ctitle);
+
+    ac_svg.append('text')
+        .attr('class', 'label title')
+        .attr('x', xcenter )
+        .attr('y', (wc.ac_svg_height-(wc.ac_margin_bottom/3)) )
+        .attr('fill', getColor(parameter) )
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '20')
+        .text('Week of 1998');
+
+    ac_svg.append('text')
+        .attr('class', 'label title')
+        .attr('x', xcenter )
+        .attr('y', wc.ac_margin_top )
+        .attr('fill', getColor(parameter))
+        .attr('text-anchor', 'middle')
+        .attr('font-size', '20')
+        .text(getFriendlyName(parameter));
+
+    // average label
+    ac_svg.select('.label.base')
+      .append('text')
+      .attr('x', (wc.ac_margin_left + wc.ac_offset) )
+      .attr('y', wc.ac_yScale(avg)-5 )
+      .attr('fill', getColor(parameter))
+      .attr('text-anchor', 'start')
+      .text('average');
+
+    ac_svg.select('.label.comp')
+      .append('text')
+      .attr('x', (wc.ac_svg_width - (wc.ac_margin_right + wc.ac_offset)) )
+      .attr('y', wc.ac_yScale(avg)-5 )
+      .attr('fill', getColor(parameter))
+      .attr('text-anchor', 'end')
+      .text('average');
+
+    // average line
+    ac_svg.append('line')
+        .attr('class', 'base axis avg')
+        .attr('x1', wc.ac_margin_left )
+        .attr('x2', wc.ac_svg_width - wc.ac_margin_right )
+        .attr('y1', wc.ac_yScale(avg) )
+        .attr('y2', wc.ac_yScale(avg) )
+        .attr('stroke', color )
+        .attr('stroke-width', '2');
+
+    ac_svg.append('line')
+        .attr('class', 'comp axis avg')
+        .attr('x1', wc.ac_margin_left )
+        .attr('x2', wc.ac_svg_width - wc.ac_margin_right )
+        .attr('y1', wc.ac_yScale(avg) )
+        .attr('y2', wc.ac_yScale(avg) )
+        .attr('stroke', color )
+        .attr('stroke-width', '2');
 }
 
 function update_activity_comparison(data, parameter, color){
@@ -151,23 +200,41 @@ function update_activity_comparison(data, parameter, color){
         sum      = data.sum,
         weekdata = data.parameter;
 
-    var ctitle   = "Compared to : " + wc.dayScale(data.day) + " " + data.hour + "-"+ (data.hour+1);
+    var ctitle   = wc.dayScale(data.day) + " " + data.hour + "-"+ (data.hour+1);
 
     // secondary title is updated
-    ac_svg.select('.ctitle').remove();
+    ac_svg.select('.label.comp').remove();
+    ac_svg.select('.avg.comp').remove();
+    ac_svg.append('g').attr('class', 'label comp');
 
-    ac_svg.append('g').attr('class', 'ctitle');
+    ac_svg.append('line')
+        .attr('class', 'comp axis avg')
+        .attr('x1', wc.ac_margin_left )
+        .attr('x2', wc.ac_svg_width - wc.ac_margin_right )
+        .attr('y1', wc.ac_yScale(avg) )
+        .attr('y2', wc.ac_yScale(avg) )
+        .attr('stroke', color )
+        .attr('stroke-width', '2');
 
-    ac_svg.select('.ctitle')
+    ac_svg.select('.label.comp')
       .append('text')
-      .attr('x', (2*wc.ac_margin_left + 2*wc.ac_offset))
+      .attr('x', (wc.ac_svg_width - (wc.ac_margin_right + wc.ac_offset)) )
       .attr('y', wc.ac_margin_top)
       .attr('fill', getColor(parameter))
-      .attr('text-anchor', 'left')
+      .attr('text-anchor', 'end')
       .text( ctitle);
 
+    ac_svg.select('.label.comp')
+      .append('text')
+      .attr('x', (wc.ac_svg_width - (wc.ac_margin_right + wc.ac_offset)) )
+      .attr('y', wc.ac_yScale(avg)-5 )
+      .attr('fill', getColor(parameter))
+      .attr('text-anchor', 'end')
+      .text('average');
+
+
     // secondary bars are updated
-    comp_bars = ac_svg.select('.comp')
+    comp_bars = ac_svg.select('.comp.bar')
       .selectAll('rect')
       .data(weekdata);
 
@@ -175,8 +242,6 @@ function update_activity_comparison(data, parameter, color){
       .delay(500)
       .attr('y', function(d){ return  wc.ac_yScale(d.parameter)} )
       .attr('height', function(d){ return wc.ac_yScale(0) - wc.ac_yScale(d.parameter)})
-//      .attr('y', function(d){ return wc.ac_height - wc.ac_yScale(d.parameter)} )
-//      .attr('height', function(d){ return wc.ac_yScale(d.parameter)})
       .each("start", function(d){
         d3.select(this)
           .transition()
@@ -198,11 +263,9 @@ function update_activity_comparison(data, parameter, color){
       .append('rect')
       .attr('class', 'bar')
       .attr('x', function(d){ return wc.ac_xScale(d.week) + wc.ac_offset} )
-//      .attr('y', function(d){ return wc.ac_height - wc.ac_yScale(d.parameter)} )
       .attr('y', function(d){ return  wc.ac_yScale(d.parameter)} )
       .attr('height', function(d){ return wc.ac_yScale(0) - wc.ac_yScale(d.parameter)})
       .attr('width', wc.ac_xScale.rangeBand() )
-//      .attr('height', function(d){ return wc.ac_yScale(d.parameter)})
       .attr('stroke', 'black')
       .attr('stroke-width', '2')
       .attr('fill', color);
